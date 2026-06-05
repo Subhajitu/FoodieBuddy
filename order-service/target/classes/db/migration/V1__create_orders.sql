52620d6e059283a639ca0142c0324f6b3af86af2
subha <subha.jitu21@gmail.com>
2026-06-05 12:52:09 +0530

    Remove target artifacts from repository

--- order-service/target/classes/db/migration/V1__create_orders.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE orders (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    user_id BIGINT NOT NULL,
-    restaurant_id BIGINT NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    order_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
